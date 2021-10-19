8819da7df4b7c4e601ca966c23de46e015733e2a
SergeySalimov <salimov_s@tut.by>
2021-10-19 22:23:26 +0300

    NH-047 [hw4095] added handling of request by isomorphic-fetch, formed the response

--- hw4095/index.js
+++ hw4095/index.js
@@ -1,6 +1,7 @@
 process.title = 'postmanApp';
 const express = require('express');
 const path = require('path');
+const fetch = require("isomorphic-fetch");
 const fs = require('fs');
 const { logLineAsync } = require('../share/loggers');
 
@@ -23,10 +24,46 @@ webServer.options('/api/request', (req, res) => {
   res.send('');
 });
 
-webServer.post('/api/request', (req, res) => {
-  console.log(req.body);
+webServer.post('/api/request', async (req, res) => {
+  const { type, url, body, headers } = req.body;
   
-  res.send('got data successfully!')
+  const options = {
+    method: type,
+    credentials: "same-origin",
+  };
+  // set Headers
+  if (headers) {
+    options.headers = headers;
+  }
+  // set Body, but not for GET, if so body ignores
+  if (body && type !== 'GET') {
+    options.body = body;
+  }
+  
+  logLineAsync(`[${PORT}] proxy called for url=${url}, method ${type} ${!!headers ? 'with headers': ''} ${!!headers && !!options.body ? 'and' : ''} ${!!options.body ? 'with body': ''}`, logPath);
+  const proxy_response = await fetch( url, options);
+  const responseText = await proxy_response.text();
+  
+  const headersArr =[];
+  proxy_response.headers.forEach((value, name) => {
+    headersArr.push(`${name}: ${value}`);
+  }, this);
+  
+  const response = {
+    responseText,
+    contentType: proxy_response.headers.get('content-type'),
+    headers: headersArr,
+    status: proxy_response.status,
+    statusText: proxy_response.statusText,
+    url: proxy_response.url,
+  };
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control','public, max-age=0');
+  res.setHeader('Content-Type', 'application/text');
+  
+  res.send(JSON.stringify(response));
 });
 
 webServer.get('*', (res, req) => {
